bf6ac92312de5f19dda80ba6fc29ff0ed9c53454
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-07-21 20:25:57 -0300

    Inserido novas paginas

---
+++ src/components/ButtonShopNow.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { defineProps, useAttrs } from 'vue'
+
+const props = defineProps({
+  url: String,
+  label: String
+})
+const attrs = useAttrs()
+
+function handleClick() {
+  window.location.href = `https://checkout.stripe.com/pay/${props.url}`;
+}
+
+</script>
+
+<style scoped>
+  .button {
+    background-color: #6EC8F0;
+    width: 326px;
+    height: 44px;
+    border: none;
+    border-radius: 6px;
+    font-weight: 700;
+    font-size: 14px;
+  }
+
+  .checkout-button:hover {
+    background-color: #52b2e3;
+  }
+</style>
+
+<template>
+  <button
+    @click="handleClick"
+    class="button"
+    style="background-color: #6EC8F0; width: 326px; height: 44px; border-radius: 5px; color: #370F1E;" 
+    v-bind="attrs"
+  >
+      <span>
+        {{ label }}
+      </span>
+  </button>
+</template>
+
