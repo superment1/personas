18c3baf41b16ceeda1d5e68493b102624bfb54a7
Lucas Menezes <lsm.developer1@gmail.com>
2025-07-24 08:43:18 -0300

    feat: termsUse

--- src/components/SuperHeader.vue
+++ src/components/SuperHeader.vue
@@ -58,6 +58,11 @@
 
 <template>
   <section class="topo">
+    <a href="/" class="absolute bg-[#fff9ed] rounded-2xl left-4 top-[3.5rem] flex items-center text-sm text-[#370f1e] hover:text-gray-700">
+      <svg class="w-8 h-8 mr-[1px]" fill="none" stroke="currentColor" stroke-width="1.2" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+      </svg>
+    </a>
     <img src="@/assets/image/sleepSuperment/superment.gif" alt="Logo animado" />
   </section>
   <section class="scroll-link-products">
