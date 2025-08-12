1cbc796a24f9dd6826d854f7d260e052e2589881
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-08-12 19:52:58 -0300

    Ajustes form

--- src/components/BannerModal.vue
+++ src/components/BannerModal.vue
@@ -70,7 +70,7 @@ watch(showModal, async (isOpen) => {
       @click="closeModal"
     >          
       <div @click.stop 
-        class="relative w-full max-w-xl font-crossfit text-[34px] md:text-[40px] shadow-2xl ring-1 rounded-xl bg-[#6EC8F0] ring-black/5 pt-4 space-y-0">
+        class="relative w-full max-w-xl font-crossfit text-[34px] md:text-[40px] shadow-2xl ring-1 rounded-xl bg-[#6EC8F0] ring-black/5 pt-4 pb-[7px] space-y-0">
         <h1 id="rd-modal-title" class="text-[#370F1E] leading-[1.3] flex items-center justify-center flex-col" >
           Sign up below to get a <br> 
           <span class="tracking-wide">
@@ -97,58 +97,94 @@ watch(showModal, async (isOpen) => {
   :deep(#lead-e9a65abee360f5aa42a1 input),
   :deep(#lead-e9a65abee360f5aa42a1 select),
   :deep(#lead-e9a65abee360f5aa42a1 textarea) {
-    height: 56px; /* aumenta altura */
+    height: 44px; /* aumenta altura */
     font-size: 18px; /* aumenta tamanho do texto */
     padding: 0 14px; /* espaço interno */
     border-radius: 10px; /* opcional */
   }
+  
+ :deep(#lead-e9a65abee360f5aa42a1 input.bricks-form__input) {
+  height: 44px !important; /* ajuste o valor como preferir */
+  font-size: 18px;          /* aumenta também o tamanho do texto */
+  padding: 0 14px;          /* deixa mais espaçado nas laterais */
+  border-radius: 10px;      /* opcional, para arredondar */
+}
 
-  :deep(#lead-e9a65abee360f5aa42a1 button) {
-    background-color: #FFDC03;
-    color: #370F1E;
-    width: 100%;
-    height: 44px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 0;
-    font-size: 22px;
-    font-style: italic;
-    font-weight: 500;
-  }
+
+/* Botão do RD */
+:deep(#lead-e9a65abee360f5aa42a1 button) {
+  background-color: #FFDC03;
+  color: #370F1E;
+  width: 100% !important;  
+  padding: 0 16px;          /* ajuste lateral */
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  font-size: 22px;
+  font-style: italic;
+  font-weight: 500;
+  border-radius: 6px;
+  align-self: flex-start;    /* garante que fique à esquerda no container */
+  margin: 5px 0 0 0;  
+}
+
 
   :deep(#rd-form-me7bf6ln) {
     width: 100% !important;   /* ocupa toda a largura do modal */
     max-width: 700px !important;
+    border-radius: 4px;
   }
 
+  :deep(#lead-e9a65abee360f5aa42a1 .phone-country .select2-choice) {
+    padding-left: 0px !important; /* ajuste fino */
+  }
+
+  /* Ajusta o select de país */
+:deep(#lead-e9a65abee360f5aa42a1 .phone-country .select2-choice) {
+  height: 44px !important; /* mesmo valor dos outros inputs */
+  display: flex;
+  align-items: center;
+  padding-left: 8px; /* espaço interno */
+}
+
+/* Ajusta o input de telefone */
+:deep(#lead-e9a65abee360f5aa42a1 .phone-input-group input) {
+  height: 44px !important;
+  font-size: 18px;
+  padding: 0 14px;
+}
+
+/* Remove espaço extra entre select e input */
+:deep(#lead-e9a65abee360f5aa42a1 .phone-input-group) {
+  gap: 0 !important;
+}
+
+  
   :deep(#rd-section-me7bf6lg) {
-    width: 100% !important;   /* ocupa toda a largura do modal */
-    max-width: 315px;
+    background: none;
+    width: 100% !important;  
   }
-
+  
   :deep(#lead-e9a65abee360f5aa42a1 .bricks-form__label) {
-    margin-bottom: 0px !important; /* padrão do RD é uns 8px~12px */
+    margin-bottom: 0px !important;
   }
 
   :deep(#lead-e9a65abee360f5aa42a1 .bricks-form__field) {
-    margin-bottom: 3px !important; /* padrão é maior */
+    margin-bottom: 4px !important;
   }
 
-  :deep(#lead-e9a65abee360f5aa42a1 button[type="submit"]) {
-    margin-top: 20px !important; /* reduz distância */
-  }
- 
   @media(min-width: 420px) {
     :deep(#rd-section-me7bf6lg) {
-      width: 100% !important;   /* ocupa toda a largura do modal */
+      width: 100% !important;   
       max-width: 355px;
     }
   }
   
   @media(min-width: 750px) {
     :deep(#rd-section-me7bf6lg) {
-      width: 100% !important;   /* ocupa toda a largura do modal */
+      width: 100% !important;   
       max-width: 600px;
     }
 
