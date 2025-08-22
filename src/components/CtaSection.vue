41d780c1614a5777ab7080943012904febf05914
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-08-22 11:59:31 -0300

    Corrigido beauty

--- src/components/CtaSection.vue
+++ src/components/CtaSection.vue
@@ -44,7 +44,8 @@
                                     d="M7.81147 20.1592L6.02697 17.1517L2.63847 16.4092L2.96922 12.921L0.673218 10.294L2.96922 7.66696L2.63847 4.17871L6.02697 3.43621L7.81147 0.428711L11 1.78246L14.1885 0.428711L15.973 3.43621L19.3615 4.17871L19.0307 7.66696L21.3267 10.294L19.0307 12.921L19.3615 16.4092L15.973 17.1517L14.1885 20.1592L11 18.8055L7.81147 20.1592ZM9.94997 13.4977L15.2537 8.19396L14.2 7.10946L9.94997 11.3595L7.79997 9.24021L6.74622 10.294L9.94997 13.4977Z"
                                     fill="#D2F58C" />
                             </svg>
-                            <span class="ml-2 leading-[1.2] font-DMSans text-[16px] font-light">Trusted U.S. manufacturing, under FDA and GMP certification</span>
+                            <span class="ml-2 leading-[1.2] font-DMSans text-[16px] font-light">Trusted U.S.
+                                manufacturing, under FDA and GMP certification</span>
                         </div>
                     </div>
                     <div class="mb-16">
@@ -69,17 +70,17 @@
                     </div>
 
                     <!-- Right Side - Content -->
-                    <div class="text-left w-[380px] h-full">
-                        <p class="text-[#A0D7FF] text-2xl font-medium italic font-gelasio mb-0 leading-[1.2]">
-                            Sleep naturally. Wake up beautiful. Every day.
+                    <div class="text-left w-[380px] h-[510px]">
+                        <p class="text-[#A0D7FF] text-[24px] font-medium italic font-gelasio mb-0 leading-[1.2]">
+                            Sleep naturally. Wake up <br>beautiful. Every day.
                         </p>
-                        <h2 class="text-white text-[44px] font-bold italic font-gelasio mb-4">
+                        <h2 class="text-white text-[44px] font-bold italic font-gelasio mb-[8px]">
                             SHOP NOW
                         </h2>
-                        <div class="inline-block mb-4">
+                        <div class="inline-block mb-[8px]">
                             <img src="@/assets/image/sleepWomam/stripe_logo.svg" class="w-[150px]" />
                         </div>
-                        <div class="space-y-1 mb-8 text-white text-lg">
+                        <div class="space-y-1 mb-[16px] text-white text-lg">
                             <div class="flex items-center">
                                 <svg width="22" height="21" viewBox="0 0 22 21" class="w-6 h-6 mr-3" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -122,7 +123,7 @@
                         </div>
                         <ShopButton id="buy-button" productId="prod_SbKYsQrxStW8wB" textColorClass="text-[#58663a]"
                             :showIcon=true
-                            class="mt-0 w-[342px] text-white justify-center justify-between bg-[#d2f58c] py-3 rounded-full text-[1.1rem] font-light hover:bg-[#58663a]/50 transition text-[22px]">
+                            class="mt-0 w-[311px] lg:w-[342px] text-white justify-center justify-between bg-[#d2f58c] py-3 rounded-full text-[18px] font-light hover:bg-[#58663a]/50 transition lg:text-[20px]">
                             Shop: First Trial Special Offer
                         </ShopButton>
                     </div>
