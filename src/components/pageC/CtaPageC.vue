933aa5cf0f24cb82bdb874627a41a19622766c07
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-08-29 16:50:12 -0300

    Criado cta page c

---
+++ src/components/pageC/CtaPageC.vue
@@ -0,0 +1,34 @@
+<template>
+    <div
+        class="w-full bg-[url('@/assets/image/pagec/banner-produto.webp')] h-[797px] flex flex-col items-center justify-start">
+        <div class="w-[335px] flex flex-col items-center justify-start pb-10 pt-[213px]">
+            <span class="text-start w-[332px] font-gelasio text-[15px] text-[#370F1E] leading-[1.18]">
+                <span class="font-bold">That’s why we created Super <br>Natural Sleep</span> — a plant-based <br>formula
+                designed to restore deep,
+                <br>natural
+                sleep <span class="font-bold">without <br>chemicals, melatonin, or <br>morning-after fog.</span>
+            </span>
+            <div class="py-8 w-full flex flex-row items-center justify-start gap-1">
+                <img src="@/assets/image/pagec/selo1.png" alt="selo">
+                <img src="@/assets/image/pagec/selo2.png" alt="selo">
+                <img src="@/assets/image/pagec/selo3.png" alt="selo">
+                <img src="@/assets/image/pagec/selo4.png" alt="selo">
+                <img src="@/assets/image/pagec/selo5.png" alt="selo">
+            </div>
+            <span class="font-gelasio italic text-[24px] text-[#370F1E] leading-[1] text-start w-[332px]">
+                A smarter approach
+                <br>to sleep better.
+                <br><span class="text-[white]">Compare and prove it:</span>
+            </span>
+            <div class="py-7">
+                <img src="@/assets/image/pagec/table.png" alt="table"></img>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
---
+++ src/components/pageC/IngredientsC.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-full flex flex-col items-center justify-start bg-[#FFFAF0]">
+        <div class="w-[310px] flex flex-col items-center justify-start py-[42px] gap-y-3">
+            <span class="font-gelasio text-[25px] leading-[1.1] text-[#370F1E] text-center">What’s inside and <br><span
+                    class="font-bold">why it
+                    works:</span></span>
+            <div class="w-full flex flex-col items-center gap-[12px]">
+                <img src="@/assets/image/pagec/flower.png" alt="">
+                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">California Poppy <br>&
+                    Passionflower</span>
+                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center">Calms mental chatter and
+                    supports <br>deep relaxation without REM disruption.</span>
+            </div>
+            <div class="w-full flex flex-col items-center gap-[12px]">
+                <img src="@/assets/image/pagec/marshmallow.png" alt="">
+                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">Marshmallow Root</span>
+                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center font-normal">Relieves
+                    nasal and
+                    throat dryness, <br>calms reflux, and supports smooth <br>breathing while you sleep.</span>
+            </div>
+            <div class="w-full flex flex-col items-center gap-[12px]">
+                <img src="@/assets/image/pagec/purple.png" alt="">
+                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">Corydalis</span>
+                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center font-normal">Eases
+                    physical tension and supports <br>smoother transitions into sleep.</span>
+            </div>
+            <div class="w-full flex flex-col items-center gap-[12px]">
+                <img src="@/assets/image/pagec/pear.png" alt="">
+                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">Prickly Pear</span>
+                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center font-normal">Reduces
+                    overnight inflammation <br>and oxidative stress.</span>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
