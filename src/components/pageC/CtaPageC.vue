8703a08ab22f1c7b0ecf33a2f9f98d51f952d08c
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-09-01 19:43:26 -0300

    Corrigido cta page c

--- src/components/pageC/CtaPageC.vue
+++ src/components/pageC/CtaPageC.vue
@@ -1,26 +1,21 @@
 <template>
     <div
-        class="w-full bg-[url('@/assets/image/pagec/banner_produto_3.webp')] h-[797px] flex flex-col items-center justify-start">
-        <div class="w-[335px] flex flex-col items-center justify-start pb-10 pt-[213px]">
-            <span class="text-start w-[332px] font-gelasio text-[15px] text-[#370F1E] leading-[1.18]">
-                <span class="font-bold">That’s why we created Super <br>Natural Sleep</span> — a plant-based <br>formula
-                designed to restore deep,
-                <br>natural
-                sleep <span class="font-bold">without <br>chemicals, melatonin, or <br>morning-after fog.</span>
+        class="w-full bg-[url('@/assets/image/pagec/banner-produto-3.webp')] bg-no-repeat bg-contain bg-cover w-[425px] flex flex-col items-center justify-start">
+        <div class="w-[355px] flex flex-col items-center justify-start pb-10 pt-[250px]">
+            <span class="text-start font-gelasio text-[15px] text-[#370F1E] leading-[1.18] w-full px-3 font-normal">
+                <span class="font-bold">That’s why we created Super <br>Natural Sleep</span> — <br>a plant-based formula
+                designed <br> to restore deep,
+                natural
+                sleep <span class="font-bold"><br> without chemicals, melatonin, <br>or morning-after fog.</span>
             </span>
-            <div class="py-8 w-full flex flex-row items-center justify-start gap-1">
+            <div class="pt-3 pb-8 w-full flex flex-row items-center justify-start gap-1">
                 <img src="@/assets/image/pagec/selo1.png" alt="selo">
                 <img src="@/assets/image/pagec/selo2.png" alt="selo">
                 <img src="@/assets/image/pagec/selo3.png" alt="selo">
                 <img src="@/assets/image/pagec/selo4.png" alt="selo">
                 <img src="@/assets/image/pagec/selo5.png" alt="selo">
             </div>
-            <span class="font-gelasio italic text-[24px] text-[#370F1E] leading-[1] text-start w-[332px]">
-                A smarter approach
-                <br>to sleep better.
-                <br><span class="text-[white]">Compare and prove it:</span>
-            </span>
-            <div class="py-7">
+            <div class="py-3">
                 <img src="@/assets/image/pagec/tabela.webp" alt="table"></img>
             </div>
         </div>
