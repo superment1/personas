be2706528d4d098fdd64241557e32e2310b0439e
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-09-02 19:20:36 -0300

    Alterado imagens page c

--- src/components/pageC/IngredientsC.vue
+++ src/components/pageC/IngredientsC.vue
@@ -1,42 +1,57 @@
 <template>
     <div class="w-full relative flex flex-col items-center justify-start bg-[#FFFAF0]">
-        <div class="w-[310px] flex flex-col items-center justify-start py-[42px] gap-y-3">
-            <span class="font-gelasio text-[25px] leading-[1.1] text-[#370F1E] text-center">What’s inside and <br><span
-                    class="font-bold">why it
+        <div
+            class="w-[310px] flex flex-col items-center justify-start py-[42px] gap-y-3 lg:w-[1100px] lg:grid lg:grid-cols-2 lg:pt-[130px] lg:pb-[85px] lg:gap-y-[40px]">
+            <span
+                class="font-gelasio text-[25px] leading-[1.1] text-[#370F1E] text-center lg:text-[52px] lg:w-full lg:col-span-2 lg:pb-[45px]">What’s
+                inside
+                and <br class="block lg:hidden"><span class="font-bold">why it
                     works:</span></span>
-            <div class="w-full flex flex-col items-center gap-[12px]">
-                <img src="@/assets/image/pagec/flower.png" alt="">
-                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">California Poppy <br>&
+            <div class="w-full flex flex-col items-center gap-[12px] lg:w-[550px] lg:h-[550px]">
+                <img src="@/assets/image/pagec/flower.png" alt="" class="w-[200px] h-[133px] lg:w-[375px] lg:h-[249px]">
+                <span
+                    class="font-gelasio text-[25px] lg:text-[44px] text-[#370F1E] text-center leading-[1.1] lg:pt-[60px]">California
+                    Poppy <br>&
                     Passionflower</span>
-                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center">Calms mental chatter and
-                    supports <br>deep relaxation without REM disruption.</span>
+                <span
+                    class="font-DMSans text-[15px] lg:text-[29px] leading-[1.18] text-[#370F1E] text-center lg:pt-[25px] lg:w-[590px] lg:mr-4">Calms
+                    mental chatter and
+                    supports <br class="block lg:hidden">deep relaxation without REM disruption.</span>
             </div>
-            <div class="w-full flex flex-col items-center gap-[12px]">
-                <img src="@/assets/image/pagec/marshmallow.png" alt="">
-                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">Marshmallow Root</span>
-                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center font-normal">Relieves
+            <div class="w-full flex flex-col items-center gap-[12px] lg:w-[550px] lg:h-[550px] lg:pt-[95px]">
+                <img src="@/assets/image/pagec/marshmallow.png" alt="" class="w-[262px] h-[153px] lg:w-[410px] ">
+                <span
+                    class="font-gelasio text-[25px] lg:text-[44px] text-[#370F1E] text-center leading-[1.1] lg:pt-[80px]">Marshmallow
+                    Root</span>
+                <span
+                    class="font-DMSans text-[15px] lg:text-[29px] leading-[1.18] text-[#370F1E] text-center lg:pt-[25px] font-normal">Relieves
                     nasal and
                     throat dryness, <br>calms reflux, and supports smooth <br>breathing while you sleep.</span>
             </div>
-            <div class="w-full flex flex-col items-center gap-[12px]">
-                <img src="@/assets/image/pagec/purple.png" alt="">
-                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">Corydalis</span>
-                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center font-normal">Eases
+            <div class="w-full flex flex-col items-center gap-[12px] lg:w-[550px] lg:h-[550px] ">
+                <img src="@/assets/image/pagec/purple.png" alt="" class="w-[102px] h-[153px] lg:w-[170px] lg:h-[256px]">
+                <span
+                    class="font-gelasio text-[25px] lg:text-[44px] text-[#370F1E] text-center leading-[1.1] lg:pt-[40px] ">Corydalis</span>
+                <span
+                    class="font-DMSans text-[15px] lg:text-[29px] leading-[1.18] text-[#370F1E] text-center lg:pt-[25px] font-normal">Eases
                     physical tension and supports <br>smoother transitions into sleep.</span>
             </div>
-            <div class="w-full flex flex-col items-center gap-[12px]">
-                <img src="@/assets/image/pagec/pear.png" alt="">
-                <span class="font-gelasio text-[25px] text-[#370F1E] text-center leading-[1.1]">Prickly Pear</span>
-                <span class="font-DMSans text-[15px] leading-[1.18] text-[#370F1E] text-center font-normal">Reduces
+            <div class="w-full flex flex-col items-center gap-[12px] lg:w-[550px] lg:h-[550px] ">
+                <img src="@/assets/image/pagec/pear.png" alt="" class="w-[123px] h-[129px] lg:w-[215px] lg:h-[222px]">
+                <span
+                    class="font-gelasio text-[25px] lg:text-[44px] text-[#370F1E] text-center leading-[1.1] lg:pt-[75px] ">Prickly
+                    Pear</span>
+                <span
+                    class="font-DMSans text-[15px] lg:text-[29px] leading-[1.18] text-[#370F1E] text-center lg:pt-[25px] font-normal">Reduces
                     overnight inflammation <br>and oxidative stress.</span>
             </div>
 
         </div>
-        <span
-            class="pointer-events-none absolute left-1/2 -translate-x-1/2 -bottom-4 translate-y-[1px] z-10
-                  w-0 h-0 border-l-[30px] border-l-transparent border-r-[30px] border-r-transparent
-                  border-t-[28px] border-t-[#FFFAF0]">
-          </span>
+        <span class="pointer-events-none absolute left-1/2 -translate-x-1/2 -bottom-4 translate-y-[1px] lg:translate-y-[10px] z-10
+                  w-0 h-0 border-l-[30px] lg:border-l-[50px] border-l-transparent border-r-[30px] lg:border-r-[50px] border-r-transparent
+                  border-t-[28px] lg:border-t-[40px] border-t-[#FFFAF0]">
+        </span>
+
     </div>
 </template>
 
