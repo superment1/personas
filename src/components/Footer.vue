05cc0186cf5f876f121f00d7ab9c3b3f9aa351ab
Lucas Menezes <lsm.developer1@gmail.com>
2025-09-19 12:49:39 -0300

    feat: slim last version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
-  <footer :class="['relative z-10 rounded-t-3xl']" :style="{ backgroundColor: color }">
+  <footer :class="['relative rounded-t-3xl']" :style="{ backgroundColor: color }">
     <div class="w-full h-[215px] flex flex-col justify-start items-center">
       <h2
         id="footer-title"
         class="text-white font-crossfit text-center mt-8 font-semibold text-[40px] sm:text-[20px] leading-tight"
-        :style="{ color: titleColor || colortext }"
+        :style="{ color: colortext }"
       >
         {{ title }}
       </h2>
       <p
         v-if="subtitle"
         class="text-white/80 text-center mt-3 font-light text-[14px] sm:text-[16px] leading-[1.15]"
-        :style="{ color: titleColor || colortext }"
+        :style="{ color: colortext }"
         v-html="subtitle"
       >
 
