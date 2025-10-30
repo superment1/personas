426bf5a9205b3834a4dc812309136f20083419b0
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2025-10-30 16:57:49 -0300

    Corrigido imagem

--- src/pages/superRelax.vue
+++ src/pages/superRelax.vue
@@ -669,7 +669,7 @@ const faqItems = [
   </section>
   <section class="bg-[#FFDC03] px-[40px] lg:px-40 pb-[42px]">
     <div class="justify-self-center max-w-[950px]">
-      <img class="justify-self-center lg:hidden" src="/assets/super_nerve.webp" alt="" loading="lazy">
+      <img class="justify-self-center lg:hidden h-[264px]" src="/assets/super_nerve.webp" alt="" loading="lazy">
       <img class="justify-self-center hidden lg:block" src="/assets/super_nerve_desk.webp" alt="" loading="lazy">
       <div>
         <p
